2983c417fd5230654877edb7917e80c874c5b34f
adityatadas24 <adityatadas2000@gmail.com>
2023-12-19 22:03:39 +0530

    submitting code-Tue Dec 19 2023 22:03:38 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
 
-import React from "react";
-import './../styles/App.css';
+import React,{useState,useEffect} from "react";
+import "./../styles/App.css";
+
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
 
 const App = () => {
+  const [fruit, setFruit] = useState("");
+  const [fruitsFilter, setFruitsFilter] = useState(fruits);
+
+  useEffect(() => {
+    handelSubmit();
+  }, [fruit]);
+
+  function handelSubmit(e) {
+    setFruitsFilter(
+      fruits.filter(
+        (item) =>
+          item.toLowerCase().includes(fruit.toLowerCase().trim()) == true
+      )
+    );
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      <h1>Search item</h1>
+      <form>
+        <input
+          type="text"
+          value={fruit}
+          onChange={(e) => setFruit(e.target.value)}
+        />
+        {/* <button >Submit</button> */}
+      </form>
 
-export default App
+      <ul>
+        {fruitsFilter.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
